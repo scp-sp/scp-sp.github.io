0ea12bcee71574f9345c3dbf82d544422a7a260e
BlackWing <blackwing@vk.com>
2024-08-13 19:17:39 +0400

    Use media query

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 window.addEventListener("scroll", () => {
 	document.getElementById("banner").style.top = window.scrollY * 0.5 + "px"
 })
-
-if (window.innerHeight > window.innerWidth) {
-    document.getElementById("content").style.gridTemplateColumns = "auto"
-    document.getElementById("sidebar").style.position = "static"
-}
