c7011a8b2a2139de66cad5d7d88b221a657f0b79
BlackWing <blackwing@vk.com>
2024-09-03 11:02:35 +0400

    Use URL parameters

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.addEventListener("scroll", () => {
 	document.getElementById("banner").style.top = window.scrollY * 0.5 + "px"
 })
+
+const params = new URLSearchParams(window.location.search);
+const access = params.get("access");
+
+if (access != 1) {
+	document.querySelectorAll(".private").forEach(element => {
+		element.remove();
+	});
+} else {
+	document.querySelectorAll("a").forEach(anchor => {
+		if (anchor.href.endsWith(".html")) {
+	    	anchor.href += "?access=1";
+		}
+	});
+
+	document.querySelectorAll(".public").forEach(element => {
+		element.remove();
+	});
+}
